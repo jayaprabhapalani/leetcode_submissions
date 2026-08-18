c66496fb837aac9504cffe026bf7b2f161f5fb45
Jayaprabha <146448216+jayaprabhapalani@users.noreply.github.com>
2026-08-18 23:45:12 +0530

    Time: 6 ms (58.77%), Space: 20.4 MB (45.95%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,15 +1,15 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
 
+        total=sum(nums) # from this we can total sum that will give as a adv of r-l summation so we can just dereive the formula
+        #left_sum+right_sum+num=total
+        #right_sum=total-left_sum-num
+        # for pivot left_sum == right_sum
+        # so the formula changes to
+        #left_sum=total-left_sum-num
+        left_sum=0
         for i in range(len(nums)):
-            left_sum=sum(nums[:i])
-            right_sum=sum(nums[i+1:])
-
-            if left_sum==right_sum:
+            if left_sum==total-left_sum-nums[i]:
                 return i
+            left_sum+=nums[i]
         return -1
-
-
-
-
-        
